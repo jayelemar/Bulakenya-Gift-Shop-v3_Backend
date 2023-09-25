db898a1a42ba018a9482f2637bebcf352825a70d
Jay Termulo <jetermulo@gmail.com>
2023-09-26 03:28:21 +0800

    build: mongoDB connection to my app

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,17 @@
+const dotenv = require("dotenv").config();
+
 const express = require("express");
+const connectDB = require('./config/connectDB');
 const app = express();
+
+connectDB();
+
+app.get('/', (req, res) => {
+    res.send("Home page");
+})
+
 const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
