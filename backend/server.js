af91f0059ac66dcdf4253c5d78843180c588d398
Jay Termulo <jetermulo@gmail.com>
2023-09-30 09:13:07 +0800

    feat: added cors

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,11 @@ connectDB();
 
 app.use(express.json())
 app.use(express.urlencoded({ extended:false }));
+
+app.use(cors({
+    origin: ["http://localhost:3000", "https://front-end_site.onrender.com"]
+}))
+
 app.use("/api/products", productRoutes);
 
 
